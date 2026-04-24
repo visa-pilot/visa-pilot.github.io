54e4852b9ff9af07529b42c5aff37cfa0263ec06
Armstrong Yan <yanqiangsdu@gmail.com>
2026-04-24 16:57:51 +0800

    style(faq): remove nav buttons and show question above answer

--- themes/hugoplate/assets/js/main.js
+++ themes/hugoplate/assets/js/main.js
@@ -1,35 +1,4 @@
 // main script
 (function () {
   "use strict";
-
-  // FAQ slider (native horizontal scroll)
-  // ----------------------------------------
-  const faqSlider = document.querySelector(".faq-slider");
-  const faqPrev = document.querySelector(".faq-slider-prev");
-  const faqNext = document.querySelector(".faq-slider-next");
-  const faqFirstCard = faqSlider ? faqSlider.querySelector(".faq-slide") : null;
-
-  if (faqSlider && faqFirstCard) {
-    const getStep = () => {
-      const styles = window.getComputedStyle(faqFirstCard);
-      const cardWidth = faqFirstCard.getBoundingClientRect().width;
-      const gap = parseFloat(styles.marginRight) || 24;
-      return cardWidth + gap;
-    };
-
-    const scrollByStep = (direction) => {
-      faqSlider.scrollBy({
-        left: direction * getStep(),
-        behavior: "smooth",
-      });
-    };
-
-    if (faqPrev) {
-      faqPrev.addEventListener("click", () => scrollByStep(-1));
-    }
-
-    if (faqNext) {
-      faqNext.addEventListener("click", () => scrollByStep(1));
-    }
-  }
 })();
